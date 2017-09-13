7b756dcab26544e427b7eb05c306278752c60e58
Justin Busschau <jubu@nodesagency.com>
2017-09-13 15:05:19 +0100

    Named Users not supported on Windows

--- src/Providers/UrbanAirshipV3.php
+++ src/Providers/UrbanAirshipV3.php
@@ -242,6 +242,10 @@ class UrbanAirshipV3 extends AbstractProvider
             throw new InvalidAudienceException('Send push to either named users or aliases - not both.');
         }
 
+        if (in_array('wns', $this->getPlatforms()) AND !empty($this->getNamedUsers())) {
+            throw new InvalidAudienceException('Push to Named Users is not supported on Windows.');
+        }
+
         // Check kb size
         if (mb_strlen(json_encode($this->buildIOSData())) > 2048) {
             throw new PushSizeLimitException(sprintf('Limit of ios is 2048b, %s was send',
